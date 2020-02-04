3eeece7eeb7615829398a5b69769d2f1468a4304
Viktor Claesson <claesson.viktor@gmail.com>
2020-02-04 13:47:29 +0100

    formSelect

--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -4,66 +4,54 @@ class ComposeSalad extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.onFormChange = this.onFormChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormChange(key, event) {
+    this.setState({
+      [key]: event.target.value
+    });
   }
 
   onFormSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    console.log(this.state);
+  }
+
+  formSelect(key) {
+    const inventory = this.props.inventory;
+    const arr = Object.keys(inventory).filter(name => inventory[name][key]);
+
+    return (
+      <div className="form-group">
+        <label>{key[0].toUpperCase() + key.slice(1)}</label>
+        <select
+          id={key}
+          onChange={event => this.onFormChange(key, event)}
+          className="form-control"
+          value={this.state.value}
+        >
+          <option disabled selected value style={{ display: "none" }}>
+            -- select an option --
+          </option>
+          {arr.map(name => (
+            <option key={name}>{name}</option>
+          ))}
+        </select>
+      </div>
+    );
   }
 
   render() {
-    const inventory = this.props.inventory;
-    let foundations = Object.keys(inventory).filter(
-      name => inventory[name].foundation
-    );
-    let proteins = Object.keys(inventory).filter(
-      name => inventory[name].protein
-    );
-    let extras = Object.keys(inventory).filter(name => inventory[name].extra);
-    let dressings = Object.keys(inventory).filter(
-      name => inventory[name].dressing
-    );
-
     return (
       <div className="container">
-        <ul>
-          {foundations.map(name => (
-            <li key={name}>{name}</li>
-          ))}
-        </ul>
         <form onSubmit={this.onFormSubmit}>
-          <div class="form-group">
-            <label for="foundation">Foundation</label>
-            <select id="foundation" className="form-control">
-              {foundations.map(name => (
-                <option key={name}>{name}</option>
-              ))}
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="protein">Protein</label>
-            <select id="protein" className="form-control">
-              {proteins.map(name => (
-                <option key={name}>{name}</option>
-              ))}
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="extra">Extra</label>
-            <select id="extra" className="form-control">
-              {extras.map(name => (
-                <option key={name}>{name}</option>
-              ))}
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="dressing">Dressing</label>
-            <select id="dressing" className="form-control">
-              {dressings.map(name => (
-                <option key={name}>{name}</option>
-              ))}
-            </select>
-          </div>
+          {this.formSelect("foundation")}
+          {this.formSelect("protein")}
+          {this.formSelect("extra")}
+          {this.formSelect("dressing")}
           <input type="submit" className="btn btn-primary" value="Submit" />
         </form>
       </div>
