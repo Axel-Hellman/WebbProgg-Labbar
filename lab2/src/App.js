17b35d93260f82373590c58c058bb016fc6063a5
Axel-Hellman <axel.hellman@outlook.com>
2020-02-05 17:16:22 +0100

    finnish lab

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import inventory from "./inventory.ES6";
 import ComposeSalad from "./ComposeSalad";
+import ViewSalad from "./ViewSalad";
 
-function App() {
-  return (
-    <div>
-      <div className="jumbotron text-center">
-        <h1>My Own Salad Bar</h1>
-        <p>Here you can order custom made salads!</p>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { salads: [] };
+
+    this.addSalad = this.addSalad.bind(this);
+  }
+
+  addSalad(salad) {
+    console.log(salad);
+    this.setState({ salads: this.state.salads.concat(salad) });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron text-center">
+          <h1>My Own Salad Bar</h1>
+          <p>Here you can order custom made salads!</p>
+        </div>
+        <ComposeSalad inventory={inventory} parentCallback={this.addSalad} />
+        <br />
+        <ViewSalad salads={this.state.salads} />
       </div>
-      <ComposeSalad inventory={inventory}></ComposeSalad>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import Salad from "./Salad";
 
 class ComposeSalad extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { extra: {} };
 
-    this.onFormChange = this.onFormChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onFormSelectChange = this.onFormSelectChange.bind(this);
+    this.onFormCheckboxChange = this.onFormCheckboxChange.bind(this);
   }
 
-  onFormChange(key, event) {
+  onFormSelectChange(key, event) {
     this.setState({
       [key]: event.target.value
     });
   }
 
+  onFormCheckboxChange(key, item, event) {
+    console.log(key, item, event.target.value);
+    this.setState({
+      [key]: { [item]: event.target.value, ...this.state[key] }
+    });
+  }
+
   onFormSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
+    const salad = new Salad(
+      this.state.foundation,
+      this.state.protein,
+      this.state.extra,
+      this.state.dressing
+    );
+    this.setState({
+      foundation: undefined,
+      protein: undefined,
+      extra: {},
+      dressing: undefined
+    });
+    this.props.parentCallback(salad);
+    event.target.reset();
   }
 
   formSelect(key) {
@@ -29,17 +51,42 @@ class ComposeSalad extends Component {
         <label>{key[0].toUpperCase() + key.slice(1)}</label>
         <select
           id={key}
-          onChange={event => this.onFormChange(key, event)}
+          onChange={event => this.onFormSelectChange(key, event)}
           className="form-control"
           value={this.state.value}
         >
-          <option disabled selected value style={{ display: "none" }}>
-            -- select an option --
+          <option selected value style={{}}>
+            None
           </option>
           {arr.map(name => (
             <option key={name}>{name}</option>
           ))}
         </select>
+      </div>
+    );
+  }
+
+  formCheckbox(key) {
+    const inventory = this.props.inventory;
+    const arr = Object.keys(inventory).filter(name => inventory[name][key]);
+
+    return (
+      <div>
+        <label>{key[0].toUpperCase() + key.slice(1) + "s"}</label>
+        <div>
+          {arr.map(item => (
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                name={item}
+                type="checkbox"
+                checked={this.state[key][item]}
+                onChange={event => this.onFormCheckboxChange(key, item, event)}
+              />
+              <label className="form-check-label">{item}</label>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
@@ -50,7 +97,7 @@ class ComposeSalad extends Component {
         <form onSubmit={this.onFormSubmit}>
           {this.formSelect("foundation")}
           {this.formSelect("protein")}
-          {this.formSelect("extra")}
+          {this.formCheckbox("extra")}
           {this.formSelect("dressing")}
           <input type="submit" className="btn btn-primary" value="Submit" />
         </form>
