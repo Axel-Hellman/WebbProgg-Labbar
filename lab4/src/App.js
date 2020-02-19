bf0b0d0affe04c19ee8f5a5e6e373e53f0f6c914
Axel-Hellman <axel.hellman@outlook.com>
2020-02-19 17:36:02 +0100

    Finnish lab4

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import ComposeSalad from "./ComposeSalad";
 import ViewSalad from "./ViewSalad";
+import Salad from "./Salad";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { orders: [], inventory: {} };
+    this.state = { orders: [], inventory: {}, loading: true };
 
     this.addSalad = this.addSalad.bind(this);
   }
@@ -32,16 +39,53 @@ class App extends Component {
       .then(data => this.objectFlattner(data));
   }
 
-  componentDidMount() {
+  fetchInventory() {
     const types = ["foundations", "proteins", "extras", "dressings"];
 
     Promise.all(types.map(type => this.fetchByType(type)))
       .then(data => this.objectFlattner(data))
-      .then(data => this.setState({ inventory: data }));
+      .then(inventory => this.setState({ inventory, loading: false }));
+  }
+
+  fetchServerOrders() {
+    fetch("http://localhost:8080/orders")
+      .then(res => res.json())
+      .then(data => data.map(order => order.order))
+      .then(orders => this.setState({ orders }));
+  }
+
+  fetchLocalOrders() {
+    const orders = JSON.parse(window.localStorage.getItem("orders"));
+    if (orders != null) {
+      orders.forEach(order => Object.setPrototypeOf(order, Salad));
+      this.setState({ orders });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchInventory();
+    this.fetchLocalOrders();
   }
 
   addSalad(salad) {
-    this.setState({ orders: this.state.orders.concat(salad) });
+    const newOrders = this.state.orders.concat(salad);
+    this.setState({ orders: newOrders });
+    window.localStorage.setItem("orders", JSON.stringify(newOrders));
+
+    fetch("http://localhost:8080/orders/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(salad)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Success:", data);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
   }
 
   Home() {
@@ -67,7 +111,7 @@ class App extends Component {
         </div>
 
         <Switch>
-          <Route path="/" exact component={this.Home} />
+          <Redirect from="/" to="/compose-salad" exact />
           <Route
             path="/compose-salad"
             exact
